d2ac2860cb0df36e423a3f5b3f7eecf6ce23bb90
nitin <nitin2533@gmail.com>
2025-09-06 11:08:55 +0530

    code4

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Subnet
+# Subnet11
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-website"
   resource_group_name  = azurerm_resource_group.rg.name
